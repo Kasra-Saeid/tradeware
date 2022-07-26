75989bab613d62dda9f2ab297f32faa6e0bdba37
kasrasaeed <kasra.saeed95@gmail.com>
2022-07-26 18:11:33 +0000

    fix: close under sign fixed

--- close_under.go
+++ close_under.go
@@ -17,7 +17,7 @@ func NewCloseUnder(firstValues, SecondValues []float64, indexBar int) *CloseUnde
 func (c *CloseUnder) Check() bool {
 	firstValues := c.firstValues
 	secondValues := c.secondValues
-	if firstValues[len(firstValues)-c.indexBar] > secondValues[len(secondValues)-c.indexBar] {
+	if firstValues[len(firstValues)-c.indexBar] < secondValues[len(secondValues)-c.indexBar] {
 		return true
 	}
 	return false
